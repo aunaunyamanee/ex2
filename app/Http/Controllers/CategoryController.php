7f972f4bc2030c34ba7ac9cf3dd2a87a18c38768
Aunyamanee Techa <auntecha@gmail.com>
2020-08-15 16:13:37 +0700

    hello Github

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+class HomeController extends Controllers
+{
+    public function index(){
+    }
+    public function create(){
+    }
+    public function store(){
+    }
+    public function edit(){
+    }
+    public function update(){
+    }
+    public function delete(){
+    }
+}
